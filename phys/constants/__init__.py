890dc753cc1961062ca9967f3b967e06ec912eeb
tylerreisinger <reisinger.tyler@gmail.com>
2016-10-19 16:46:33 -0400

    Initial units

---
+++ phys/constants/__init__.py
@@ -0,0 +1,11 @@
+from phys.units import u as units
+
+c = 3e8 * (units.meter / units.second)
+fine = 1/137.036
+fine_structure = fine
+
+q_e = -1.602176565e-19 * units.coulomb
+e0 = 8.854e-12 * units.farad / units.meter
+
+hbar = 1.0545718e-34 * units.joule * units.second
+boltzmann = 1.38064852e-23 * units.meter**2 * units.kilogram / (units.seconds**2 * units.kelvin)
---
+++ phys/constants/mass.py
@@ -0,0 +1,5 @@
+from phys.units import u as units
+
+m_e = 9.10938356e-31 * units.kg
+m_p = 1.6726219e-27 * units.kg
+
